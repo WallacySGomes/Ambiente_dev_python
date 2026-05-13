6a96e485e34f28610a63d3ae575fa40c0c307d48
Wallacy Santos <dev.wallacy@gmail.com>
2026-05-12 22:01:17 -0300

    Corrigido o entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
-
 set -e
 
 # Espera pelo Postgres
-until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST:$POSTGRES_PORT) ..."
   sleep 2
 done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
-# Executa comandos do Django
+# Django tasks
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-# Inicia o Gunicorn em produção
+# Gunicorn server
 exec gunicorn djangoapp.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 4 \
